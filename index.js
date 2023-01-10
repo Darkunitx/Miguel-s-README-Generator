83d51a9b7c443167183ba552f5eae38d83be7060
Darkunitx <darkunitx@live.com>
2023-01-10 12:27:21 -0600

    added prompts to get info from the user

--- index.js
+++ index.js
@@ -2,3 +2,41 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 
+const generateREADME = ({ Title, Description, installation, guidelines, test }) =>
+  `README`;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'Title',
+      message: 'What is your Project Title?',
+    },
+    {
+      type: 'input',
+      name: 'Description',
+      message: 'Please write a description',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'guidelines',
+      message: 'What are the contribution guidelines?',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Please write instructions for testing.',
+    },
+  ])
+  .then((answers) => {
+    const READMEPageContent = generateREADME(answers);
+
+    fs.writeFile('README.md', READMEPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README!')
+    );
+  });
